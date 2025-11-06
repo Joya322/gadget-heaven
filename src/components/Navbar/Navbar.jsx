5db5847dede8c3ed7d29f69a052a6a73ca23ade8
Joya322 <jannatferdoush1212@gmail.com>
2025-11-06 10:25:17 +0600

    first setup commit

---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,60 @@
+import React from "react";
+import { AiOutlineShoppingCart } from "react-icons/ai";
+import { FaRegHeart } from "react-icons/fa";
+
+const Navbar = () => {
+    const links = (<>
+        <li>
+            <a>Home</a>
+        </li>
+        <li>
+            <a>Statistics</a>
+        </li>
+        <li>
+            <a>Dashboard</a>
+        </li>
+    </>);
+    
+  return (
+    <div className="navbar bg-base-100 mb-8">
+      <div className="navbar-start">
+        <div className="dropdown">
+          <label tabIndex={0} className="btn btn-ghost lg:hidden">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="h-5 w-5"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="M4 6h16M4 12h8m-8 6h16"
+              />
+            </svg>
+          </label>
+          <ul
+            tabIndex={0}
+            className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52"
+          >
+            {links}
+          </ul>
+        </div>
+        <a className="btn btn-ghost normal-case text-xl">Gadget Heaven</a>
+      </div>
+      <div className="navbar-center hidden lg:flex">
+        <ul className="menu menu-horizontal px-1">
+          {links}
+        </ul>
+      </div>
+      <div className="navbar-end">
+              <AiOutlineShoppingCart />
+              <FaRegHeart/>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
---
+++ src/main.jsx
@@ -0,0 +1,33 @@
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import './index.css'
+
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Root from "./components/Root/Root.jsx";
+import ErrorPage from "./components/ErrorPage/ErrorPage.jsx";
+import Home from "./components/Home/Home.jsx";
+
+const router = createBrowserRouter(
+  [
+    {
+      path: "/",
+      element: <Root />,
+      errorElement: <ErrorPage />,
+      children: [
+        {
+          path: "/",
+          element: <Home />,
+        },
+      ],
+    },
+  ],
+  {
+    basename: "/gadget-heaven/",
+  }
+);
+
+createRoot(document.getElementById("root")).render(
+  <StrictMode>
+    <RouterProvider router={router} />
+  </StrictMode>
+);
