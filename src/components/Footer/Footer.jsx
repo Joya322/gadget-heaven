94ca82203f59fe462fc65730b47a04c34a22e27f
Joya322 <jannatferdoush1212@gmail.com>
2025-11-06 18:10:09 +0600

    footer done

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,30 +1,41 @@
-import React from 'react';
+import React from "react";
 
 const Footer = () => {
-    return (
-      <footer className="footer p-10 bg-neutral text-neutral-content mt-8">
-        <nav className="justify-center w-full">
-          <header className="footer-title">Services</header>
-          <a className="link link-hover">Branding</a>
-          <a className="link link-hover">Design</a>
-          <a className="link link-hover">Marketing</a>
-          <a className="link link-hover">Advertisement</a>
-        </nav>
-        <nav className="justify-center w-full">
-          <header className="footer-title">Company</header>
-          <a className="link link-hover">About us</a>
-          <a className="link link-hover">Contact</a>
-          <a className="link link-hover">Jobs</a>
-          <a className="link link-hover">Press kit</a>
-        </nav>
-        <nav className="justify-center w-full">
-          <header className="footer-title">Legal</header>
-          <a className="link link-hover">Terms of use</a>
-          <a className="link link-hover">Privacy policy</a>
-          <a className="link link-hover">Cookie policy</a>
-        </nav>
-      </footer>
-    );
+  return (
+    <footer className="py-10 bg-white mt-8">
+      <div>
+        <h1 className="text-3xl font-bold text-[rgba(9,8,15)] text-center ">
+          Gadget Heaven
+        </h1>
+        <p className="text-center text-[rgba(9,8,15,0.6)] py-5">
+          Leading the way in cutting-edge technology and innovation.
+        </p>
+        <p className="border-b-2 my-3"></p>
+        <div className="flex justify-between w-11/12 mx-auto px-10 text-black">
+          <div className="flex flex-col">
+            <h3 className="text-lg font-bold mb-2">Services</h3>
+            <a className="link link-hover opacity-60">Branding</a>
+            <a className="link link-hover opacity-60">Design</a>
+            <a className="link link-hover opacity-60">Marketing</a>
+            <a className="link link-hover opacity-60">Advertisement</a>
+          </div>
+          <div className="flex flex-col">
+            <h3 className="text-lg font-bold mb-2">Company</h3>
+            <a className="link link-hover opacity-60">About us</a>
+            <a className="link link-hover opacity-60">Contact</a>
+            <a className="link link-hover opacity-60">Jobs</a>
+            <a className="link link-hover opacity-60">Press kit</a>
+          </div>
+          <div className="flex flex-col">
+            <h3 className="text-lg font-bold mb-2">Legal</h3>
+            <a className="link link-hover opacity-60">Terms of use</a>
+            <a className="link link-hover opacity-60">Privacy policy</a>
+            <a className="link link-hover opacity-60">Cookie policy</a>
+          </div>
+        </div>
+      </div>
+    </footer>
+  );
 };
 
 export default Footer;
